3ce7fd43ff60e19b4f33470e265c8439e99ba9f8
tomiyasu yuya <tomi.tomi.tomi.09@icloud.com>
2022-07-30 14:33:12 +0900

    Add: header

---
+++ test/controllers/providers_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ProvidersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get providers_index_url
+    assert_response :success
+  end
+
+end
