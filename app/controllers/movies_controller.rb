3444b7b6cd85da0db02c3e5b03784c604151c0fe
tomiyasu yuya <tomi.tomi.tomi.09@icloud.com>
2022-09-17 10:50:13 +0900

    API_KEの環境変数を設定

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=dbe29fe1b6b50465b49fb627d73149ac&language=ja-JP&with_networks=213&with_genres=16|35&page=1")
+    uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV['API_KEY']}&language=ja-JP&with_networks=213&with_genres=16|35&page=1")
     json = Net::HTTP.get(uri)
     result = JSON.parse(json)
     @netflex = result["results"]
--- app/models/tmdb_api.rb
+++ app/models/tmdb_api.rb
@@ -5,7 +5,7 @@ class TmdbApi
       genre_id = params[:genre_id]
       provider = URI.encode_www_form({with_networks: provider_id})
       genre = URI.encode_www_form({with_genres: genre_id})
-      uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=dbe29fe1b6b50465b49fb627d73149ac&language=ja-JP&#{provider}&#{genre}&page=1")
+      uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV['API_KEY']}&language=ja-JP&#{provider}&#{genre}&page=1")
       json = Net::HTTP.get(uri)
       data_list = JSON.parse(json)
       movie = data_list["results"]
