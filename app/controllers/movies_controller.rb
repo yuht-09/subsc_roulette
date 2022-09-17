c73c191f9996e9589a5e297fabe7fe52f266cbd5
tomiyasu yuya <tomi.tomi.tomi.09@icloud.com>
2022-09-18 00:49:07 +0900

    Tmdbからデータを取得し、ランダムで表示

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all(params).sample(1)
+  end
+
+  def show
+    uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV['API_KEY']}&language=ja-JP&with_networks=213&with_genres=16|35&page=1")
+    json = Net::HTTP.get(uri)
+    result = JSON.parse(json)
+    @netflex = result["results"]
+  end
 end
