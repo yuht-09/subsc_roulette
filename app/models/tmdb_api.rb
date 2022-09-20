cf4d867f824eae736529585104bec93528cc177e
tomiyasu yuya <tomi.tomi.tomi.09@icloud.com>
2022-09-20 15:11:45 +0900

    TmdbからランダムにPageを取得

--- app/models/tmdb_api.rb
+++ app/models/tmdb_api.rb
@@ -5,7 +5,11 @@ class TmdbApi
       genre_id = params[:genre_id]
       provider = URI.encode_www_form({with_networks: provider_id})
       genre = URI.encode_www_form({with_genres: genre_id})
-      uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV['API_KEY']}&language=ja-JP&#{provider}&#{genre}&page=1")
+      uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV['API_KEY']}&language=ja-JP&#{provider}&#{genre}")
+      json = Net::HTTP.get(uri)
+      data_list = JSON.parse(json)
+      movie = []
+      uri = URI.parse("https://api.themoviedb.org/3/discover/tv?api_key=#{ENV['API_KEY']}&language=ja-JP&#{provider}&#{genre}&page=#{rand(data_list['total_pages'])+1}")
       json = Net::HTTP.get(uri)
       data_list = JSON.parse(json)
       movie = data_list["results"]
