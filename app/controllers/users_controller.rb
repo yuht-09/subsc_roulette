a89b19c4c6bc9a9e8344eb9c35fad1da41d57bd5
tomiyasu yuya <tomi.tomi.tomi.09@icloud.com>
2022-10-29 13:33:02 +0900

    ユーザー編集のフラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,9 +27,9 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to user_path(@user), success: 'ユーザーを編集しました'
+      redirect_to user_path(@user), success: t('.success')
     else
-      flash.now[:danger] = 'ユーザーの編集に失敗しました'
+      flash.now[:danger] = t('.fail')
       render :edit
     end
   end
